03f49f346602b9be6144738429112806ab138eec
imstudent2210 <imstudent2210@gmail.com>
2023-12-13 21:29:57 +0700

    vue_buoi 2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Vue.js App - Tam-NLT"/>
-
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
     <div>
       <p>Two way binding in Vue</p>
     <div v-show="isShow">
@@ -12,20 +11,55 @@
     {{ count }}
     </div>
 
+    <h2 :class="{redText: isTrue}">fesfesfetre</h2>
+    <button @click="isTrue = !isTrue">Add Class Binding</button>
+    <input type="text" v-model="arr[0]">
+
+    {{ object.id }}
+
+    <div>
+      <ul>
+        <li v-for="(item, index) in arr" :key="index">{{ item }}</li>
+      </ul>
+    </div>
+    <div>
+      <input type="text" v-model="newTask">
+      <button @click="listObject.push({ taskName: newTask, done: false })">Add task</button>
+      <div v-for="(object, index) in listObject" :key="index">
+        <input type="checkbox" v-model="object.done"> <span :class="{done: object.done }">{{ object.taskName }}</span>
+      </div>
+  </div>
+  <!-- <TodoList v-for="(object, index) in listObject" :key="index" :taskData="object"/> -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import TodoList from './components/Vue1312.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // TodoList
   },
   data () {
     return {
       isShow: true,
-      count: 0
+      count: 0,
+      isTrue: false,
+      arr: [
+        'item 1',
+        'item 2',
+        'item 3',
+        'item 4'
+      ],
+      object: {
+        id: '001'
+      },
+      newTask: '',
+      listObject: [
+        { taskName: 'task1', done: false },
+        { taskName: 'task2', done: false },
+        { taskName: 'task3', done: true },
+        { taskName: 'task4', done: false }]
     }
   },
   updated () {
@@ -41,8 +75,13 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.redText{
+  color: red;
+}
+.done{
+  text-decoration: line-through;
 }
 </style>
---
+++ src/components/Vue1312.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="checkbox" v-mode="taskData.done"> <span :class="{done: taskData.done }">{{ taskData.taskName }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: [
+    'taskData'
+  ]
+}
+</script>
+
+<style>
+.done{
+  text-decoration: line-through;
+}
+</style>
